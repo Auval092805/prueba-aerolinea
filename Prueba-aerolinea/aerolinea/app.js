fa7f2ae16562eaaca080a1f098175dca71e73143
Auval092805 <130373274+Auval092805@users.noreply.github.com>
2023-05-08 12:51:22 -0500

    Add files via upload

---
+++ Prueba-aerolinea/aerolinea/app.js
@@ -0,0 +1,4 @@
+import './Components/main-menu/main-menu.js';
+import './Components/forms/customer-form.js';
+import './Components/listas/customer-lista.js';
+import './Components/forms/personal-form.js';
